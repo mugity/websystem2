6b4572b77683ddd162c4009867ec112c8b1e5144
mugity <s1632056cj@s.chibakoudai.jp>
2019-05-22 06:39:37 +0000

    大学の割合追加

--- server8.js
+++ server8.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 server.get('/', function( req, res ) {
     let sorting = req.query.sorting || '人口';
     let number = req.query.number || 10;
-    let query = 'select id, 都道府県, ' + sorting + ' from example order by ' + sorting + ' desc limit ' + number + ';';
+    let query = 'select id, 都道府県, ' + sorting + ' as population from example order by ' + sorting + ' desc limit ' + number + ';';
     console.log( query );
     connection.query( query, (error, rows, fields) => {
         if( error ) {
