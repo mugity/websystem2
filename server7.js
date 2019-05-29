d440ba961514d5a02acb324fd7f253fdc4cbc445
mugity <49706130+mugity@users.noreply.github.com>
2019-05-29 15:49:58 +0900

    Update server7.js

--- server7.js
+++ server7.js
@@ -12,7 +12,8 @@ var connection = mysql.createConnection({
 });
 
 server.get('/', function( req, res ) {
-    connection.query('select id, 都道府県, 人口 from example order by 人口 desc limit 10;', (error, rows, fields) => {
+    let query = 'select butting.id, butting.year, butting.player_id, butting.HR limit 10;';
+    connection.query( query, (error, rows, fields) => {
         if( error ) {
             console.log('Query Error');
         }
